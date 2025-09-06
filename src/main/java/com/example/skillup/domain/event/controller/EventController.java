3b34e88399d2cb10e82d3040feffe6af7c9c295c
junwoo <mkshie@naver.com>
2025-09-06 23:55:06 +0900

    feat : 관리자 권한 확인 추가

--- src/main/java/com/example/skillup/domain/event/controller/EventController.java
+++ src/main/java/com/example/skillup/domain/event/controller/EventController.java
@@ -3,7 +3,6 @@ package com.example.skillup.domain.event.controller;
 import com.example.skillup.domain.event.dto.request.EventRequest;
 import com.example.skillup.domain.event.entity.Event;
 import com.example.skillup.domain.event.service.EventService;
-import com.example.skillup.domain.user.entity.UsersDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -13,7 +12,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/events")
@@ -23,16 +26,16 @@ public class EventController {
     private final EventService eventService;
 
     @PostMapping
-    //@PreAuthorize("hasRole('OWNER')")
+    @PreAuthorize("hasRole('OWNER')")
     @Operation(summary = "행사 등록 API", description = "관리자가 행사를 등록합니다.")
     @ApiResponse(responseCode = "200", description = "일정 등록 성공",
             content = @Content(mediaType = "application/json"))
     public ResponseEntity<String> createEvent(
-            @RequestBody @Valid EventRequest.CreateEvent request
-            //@AuthenticationPrincipal UsersDetails userDetails
+            @RequestBody @Valid EventRequest.CreateEvent request,
+            @AuthenticationPrincipal User user
     ) {
-        //Event event = eventService.createEvent(userDetails.getUser(),request);
+        //Event event = eventService.createEvent(userDetails.getUsers(),request);
         Event event = eventService.createEvent(request);
-        return ResponseEntity.ok("이벤트가 등록되었습니다." + event.getId());
+        return ResponseEntity.ok("이벤트가 등록되었습니다." + event.getId() + " " + user.getUsername());
     }
 }
