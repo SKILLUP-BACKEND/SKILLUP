2cf11d61069be06bf433ddbf4cd7019ef31c0d26
kimjunghwannn <aa2002dcks@naver.com>
2025-10-21 21:04:48 +0900

    feat: 해시태그 엔티티 및 Enum 분류 추가

--- src/main/java/com/example/skillup/domain/event/enums/ActionType.java
+++ src/main/java/com/example/skillup/domain/event/enums/ActionType.java
@@ -1,5 +1,8 @@
 package com.example.skillup.domain.event.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum ActionType {
     VIEW, SAVE, APPLY
 }
--- src/main/java/com/example/skillup/domain/event/enums/BannerType.java
+++ src/main/java/com/example/skillup/domain/event/enums/BannerType.java
@@ -1,10 +1,8 @@
 package com.example.skillup.domain.event.enums;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum BannerType {
     MAIN_BANNER,SUB_BANNER
 }
