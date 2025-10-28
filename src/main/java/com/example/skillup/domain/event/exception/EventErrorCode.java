5652f5c01b44558a7135df92591745d18e2fcc56
junwoo <mkshie@naver.com>
2025-10-29 03:20:59 +0900

    feat(enum) : Elastic 에러용 enum 추가

--- src/main/java/com/example/skillup/domain/event/exception/EventErrorCode.java
+++ src/main/java/com/example/skillup/domain/event/exception/EventErrorCode.java
@@ -11,7 +11,8 @@ public enum EventErrorCode implements ResultCode {
     EVENT_ENTITY_NOT_FOUND("EVENT_ENTITY_NOT_FOUND","행사가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
     EVENT_ALREADY_HIDDEN("EVENT_ALREADY_HIDDEN", "이미 숨김된 행사입니다.", HttpStatus.BAD_REQUEST),
     EVENT_ALREADY_PUBLISHED("EVENT_ALREADY_PUBLISHED","이미 등록된 행사입니다." ,HttpStatus.BAD_REQUEST),
-    EVENT_ALREADY_DELETED("EVENT_ALREADY_DELETED", "이미 삭제된 행사입니다.", HttpStatus.BAD_REQUEST);
+    EVENT_ALREADY_DELETED("EVENT_ALREADY_DELETED", "이미 삭제된 행사입니다.", HttpStatus.BAD_REQUEST),
+    EVENT_INDEXING_ERROR("EVENT_INDEXING_ERROR" , "행사 데이터를 인덱싱 처리 과정에서의 에러가 발생했습니다." , HttpStatus.SERVICE_UNAVAILABLE);
 
     private final String code;
     private final String message;
