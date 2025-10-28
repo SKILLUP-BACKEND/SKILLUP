c6af643ef1b0e297a14bbe5ae9ff5e765b2f022f
junwoo <mkshie@naver.com>
2025-10-29 03:26:48 +0900

    feat(Controller) : admin 기능에 초기 인덱싱 기능 api 추가

--- src/main/java/com/example/skillup/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/skillup/domain/admin/controller/AdminController.java
@@ -3,11 +3,14 @@ package com.example.skillup.domain.admin.controller;
 import com.example.skillup.domain.admin.dto.AdminLoginRequest;
 import com.example.skillup.domain.admin.entity.Admin;
 import com.example.skillup.domain.admin.service.AdminService;
+import com.example.skillup.domain.event.search.service.EventIndexerService;
 import com.example.skillup.global.auth.dto.response.TokenResponse;
 import com.example.skillup.global.auth.service.AuthService;
 import com.example.skillup.global.common.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,8 @@ public class AdminController
 {
     private final AuthService authService;
     private final AdminService adminService;
+    private final EventIndexerService eventIndexerService;
+
     @PostMapping("/login")
     public BaseResponse<TokenResponse> login(
             @RequestBody @Valid AdminLoginRequest request
@@ -30,5 +35,13 @@ public class AdminController
         TokenResponse tokenResponse = authService.login(null, admin.getRole().toString());
 
         return BaseResponse.success("관리자 로그인에 성공했습니다.",tokenResponse);
+    }
+
+    @PostMapping("search")
+    @PreAuthorize("hasRole('OWNER')")
+    @Operation(summary = "초기 존재하는 행사 인덱싱 api", description = "ElasticSearch 초기 세팅시 존재하는 행사들을 추가하는 api")
+    public BaseResponse<String>  reindexAll(){
+        long total = eventIndexerService.bulkIndexAll();
+        return BaseResponse.success("현재 존재하는 행사 값들을 인덱싱 작업을 성공했습니다." , "변환된 행사의 개수 : " + total);
     }
 }
