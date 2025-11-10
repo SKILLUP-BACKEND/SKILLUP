726abc0b6a06f94a38c98e1f2c50edc0d5ef028a
junwoo <mkshie@naver.com>
2025-11-10 20:19:10 +0900

    feat : bookmark 기능을 위한 기본적인 파일 추가
    - bookmark 엔티티 , 레포 , 서비스 추가

--- src/main/java/com/example/skillup/domain/event/service/EventBookmarkService.java
+++ src/main/java/com/example/skillup/domain/event/service/EventBookmarkService.java
@@ -5,12 +5,12 @@ import com.example.skillup.domain.event.entity.Event;
 import com.example.skillup.domain.event.entity.EventBookmark;
 import com.example.skillup.domain.event.repository.EventBookmarkRepository;
 import com.example.skillup.domain.user.entity.Users;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class EventBookmarkService {
 
     private EventBookmarkRepository eventBookmarkRepository;
