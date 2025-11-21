bc9bd94b28f8b6d6e730e1ba2e05cca326963301
kimjunghwannn <aa2002dcks@naver.com>
2025-11-22 00:12:08 +0900

    fix: 리스트를 is null로 체크하는 오류 수정 + 이벤트 카운트 쿼리 로직 수정

--- src/main/java/com/example/skillup/domain/event/repository/EventRepository.java
+++ src/main/java/com/example/skillup/domain/event/repository/EventRepository.java
@@ -274,29 +274,8 @@ public interface EventRepository extends JpaRepository<Event, Long>, EventReposi
 
 
 
-    @Query(value = """
-    SELECT COUNT(DISTINCT e.id)
-    FROM event e
-    LEFT JOIN event_target_role etr ON etr.event_id = e.id
-    LEFT JOIN target_role tr ON tr.id = etr.role_id
-    WHERE (:category IS NULL OR e.category = :category)
-      AND (e.event_end IS NULL OR e.event_end >= :now)
-      AND (e.status = 'PUBLISHED')
-      AND (:isOnline IS NULL OR e.is_online = :isOnline)
-      AND (:isFree IS NULL OR e.is_free = :isFree)
-      AND (:startDate IS NULL OR e.event_start BETWEEN :startDate AND :endDate)
-      AND (:targetRoles IS NULL OR tr.name IN (:targetRoles))
-""", nativeQuery = true)
-    int countByCategoryWithSearch(
-            @Param("category") String category,
-            @Param("isOnline") Boolean isOnline,
-            @Param("isFree") Boolean isFree,
-            @Param("startDate") LocalDateTime startDate,
-            @Param("endDate") LocalDateTime endDate,
-            @Param("targetRoles") List<String> targetRoles,
-            @Param("now") LocalDateTime now
-    );
-
+    @Query("SELECT COUNT(e) FROM Event e")
+    int countAllEvents();
 
 
     // 위에는 점수까지 포함(test 용) 아래는 점수 포함하지 않은 쿼리문
