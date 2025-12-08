eb787cf1b1343f10a2f5c8cff2b69bdfc2b6b106
kimjunghwannn <aa2002dcks@naver.com>
2025-12-08 21:14:11 +0900

    fix: getUsers에서 User를 영속성 컨텍스트로 가져오는 로직 추가

--- src/main/java/com/example/skillup/domain/user/service/UserService.java
+++ src/main/java/com/example/skillup/domain/user/service/UserService.java
@@ -116,6 +116,7 @@ public class UserService
     }
 
     public UserResponse.UserProfileResponse getUsers(Users user) {
+        user=userRepository.findById(user.getId()).orElse(null);
         return userMapper.toUserProfileResponse(user);
     }
 }
