361800e29f21822f86091946fba240b14869b66a
junwoo <mkshie@naver.com>
2025-12-10 04:49:02 +0900

    feat(user) : 프로필 수정 API S3 로직 추가
    - RequestBody -> RequestPart 로 수정
    - profileImage 변수 추가
    - User.update() 에 userProfileImageUrl 추가
    - request 의 imageUrl 제거

--- src/main/java/com/example/skillup/domain/user/service/UserService.java
+++ src/main/java/com/example/skillup/domain/user/service/UserService.java
@@ -17,75 +17,73 @@ import com.example.skillup.domain.user.entity.Users;
 import com.example.skillup.domain.user.mappers.UserMapper;
 import com.example.skillup.domain.user.repository.InquiryRepository;
 import com.example.skillup.domain.user.repository.InterestRepository;
-import com.example.skillup.domain.user.repository.UserRepository;
 import com.example.skillup.global.aop.ConvertNotFound;
 import com.example.skillup.global.common.CommonResponse;
+import com.example.skillup.global.service.S3Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class UserService
-{
+public class UserService {
     final private UserMapper userMapper;
     final private EventBookmarkRepository eventBookmarkRepository;
     final private InterestRepository interestRepository;
     final private EventMapper eventMapper;
     final private TargetRoleRepository targetRoleRepository;
     final private InquiryRepository inquiryRepository;
+    private final S3Service s3Service;
 
-    public UserResponse.MyPageHomeResponse getMyPageHome(Users user)
-    {
+    public UserResponse.MyPageHomeResponse getMyPageHome(Users user) {
         return userMapper.toMyPageHomeResponse(user);
     }
 
     @Transactional(readOnly = true)
-    public UserResponse.MyPageBookMarkResponse getMyPageBookMark(Users user, EventCategory category,String sort,int page)
-    {
-        List<Event> eventBookmarks=new ArrayList<>();
+    public UserResponse.MyPageBookMarkResponse getMyPageBookMark(Users user, EventCategory category, String sort,
+                                                                 int page) {
+        List<Event> eventBookmarks = new ArrayList<>();
         Pageable pageable = PageRequest.of(page, 9);
 
-
-
         switch (sort) {
-            case "latest" -> eventBookmarks=eventBookmarkRepository.findEventsByUserWithLatest(user, category,pageable);
-            case "deadline" -> eventBookmarks=eventBookmarkRepository.findEventsByUserWithDeadLine(user, category,pageable);
+            case "latest" ->
+                    eventBookmarks = eventBookmarkRepository.findEventsByUserWithLatest(user, category, pageable);
+            case "deadline" ->
+                    eventBookmarks = eventBookmarkRepository.findEventsByUserWithDeadLine(user, category, pageable);
         }
-        List<EventResponse.HomeEventResponse> eventBookmarksDto=eventBookmarks.stream()
+        List<EventResponse.HomeEventResponse> eventBookmarksDto = eventBookmarks.stream()
                 .map(event -> eventMapper.toFeaturedEvent(event, true, event.isRecommendedManual(), event.isAd(), null))
                 .toList();
 
         CommonResponse.PageInfoResponse pageInfoResponse = CommonResponse.PageInfoResponse
                 .builder()
-                .currentPage(page+1)
+                .currentPage(page + 1)
                 .pageSize(pageable.getPageSize())
                 .totalPages((int) Math.ceil((double) eventBookmarksDto.size() / (pageable.getPageSize())))
                 .build();
 
-        List<EventResponse.HomeEventResponse> onGoingEvents=new ArrayList<>();
-        List<EventResponse.HomeEventResponse> completedEvents=new ArrayList<>();
+        List<EventResponse.HomeEventResponse> onGoingEvents = new ArrayList<>();
+        List<EventResponse.HomeEventResponse> completedEvents = new ArrayList<>();
 
-        for(EventResponse.HomeEventResponse event:eventBookmarksDto)
-        {
-            if (event.getD_dayLabel().equals("마감"))
+        for (EventResponse.HomeEventResponse event : eventBookmarksDto) {
+            if (event.getD_dayLabel().equals("마감")) {
                 completedEvents.add(event);
-            else
+            } else {
                 onGoingEvents.add(event);
+            }
         }
-        return userMapper.toMyPageBookMarkResponse(user,onGoingEvents,completedEvents,pageInfoResponse);
+        return userMapper.toMyPageBookMarkResponse(user, onGoingEvents, completedEvents, pageInfoResponse);
     }
 
     @Transactional(readOnly = true)
-    public List<UserResponse.InterestResponse> getInterestByRole(TargetRole role)
-    {
+    public List<UserResponse.InterestResponse> getInterestByRole(TargetRole role) {
         return interestRepository.findByRole(role).stream().map(userMapper::toInterestResponse).toList();
     }
 
@@ -94,17 +92,17 @@ public class UserService
             errorCodeEnum = TargetRoleErrorCode.class,
             errorCodeName = "TARGET_ROLE_NOT_FOUND"
     )
-    @Transactional(readOnly = true)
-    public UserResponse.UserProfileResponse updateUser(Users user, UserRequest.UserUpdateRequest request)
-    {
-        TargetRole role=targetRoleRepository.findByName(request.getRole()).orElseThrow();
-        Set<Interest> interests=interestRepository.findByNameIn(request.getInterests());
-        user.update(request,role,interests);
+    @Transactional
+    public UserResponse.UserProfileResponse updateUser(Users user, UserRequest.UserUpdateRequest request,
+                                                       MultipartFile profileImage) {
+        TargetRole role = targetRoleRepository.findByName(request.getRole()).orElseThrow();
+        Set<Interest> interests = interestRepository.findByNameIn(request.getInterests());
+        String userProfileImageUrl = s3Service.uploadFile(profileImage, "user/profile");
+        user.update(request, role, interests, userProfileImageUrl);
         return userMapper.toUserProfileResponse(user);
     }
 
-    public List<UserResponse.InquiryResponse> getAllInquiry()
-    {
+    public List<UserResponse.InquiryResponse> getAllInquiry() {
         return inquiryRepository.findAll().stream().map(userMapper::toInquiryResponse).toList();
     }
 }
