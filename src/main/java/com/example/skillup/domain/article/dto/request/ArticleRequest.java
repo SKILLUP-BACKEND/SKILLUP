95e5cd36db0c9714291ccf952c71bcc56161b0aa
junwoo <mkshie@naver.com>
2025-12-15 20:48:07 +0900

    feat(article) : dto 생성 및 mapper 생성
    - AdminArticleResponseList 관리자 아티클 조회시
    - AdminArticleDetailsResponse 관리자 아티클 상세 조회
    - HomeArticleResponse 일반 사용자 아티클 조회

---
+++ src/main/java/com/example/skillup/domain/article/dto/request/ArticleRequest.java
@@ -0,0 +1,68 @@
+package com.example.skillup.domain.article.dto.request;
+
+import com.example.skillup.domain.article.enums.ArticleStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class ArticleRequest {
+
+    @Getter
+    @NoArgsConstructor
+    public static class CreateArticleRequest {
+
+        @NotBlank(message = "제목을 입력해주세요")
+        private String title;
+
+        @NotBlank(message = "아티클 내용 요약을 입력해주세요")
+        private String summary;
+
+        @NotBlank(message = "원본 링크를 입력해주세요")
+        private String originalUrl;
+
+        private ArticleStatus status = ArticleStatus.PUBLISHED;
+
+        @NotNull(message = "출처를 적어주세요")
+        private String source;
+
+        @NotNull(message = "원본의 게시글 시작일을 입력해주세요")
+        private LocalDate originalPublishedDate;
+
+        @NotNull(message = "추천 대상 최소 1개 선택해주세요.")
+        @Size(min = 1, message = "최소 1개의 추천 대상이 필요합니다.")
+        //"개발자", "AI" 등등
+        private List<String> targetRoles;
+    }
+
+
+    @Getter
+    @NoArgsConstructor
+    public static class AdminUpdateArticleRequest {
+        @NotBlank(message = "제목을 입력해주세요")
+        private String title;
+
+        @NotBlank(message = "아티클 내용 요약을 입력해주세요")
+        private String summary;
+
+        @NotBlank(message = "원본 링크를 입력해주세요")
+        private String originalUrl;
+
+        @NotNull(message = "아티클의 상태값을 선택해주세요 PUBLISHED / DRAFT")
+        private ArticleStatus status;
+
+        @NotNull(message = "출처를 적어주세요")
+        private String source;
+
+        @NotNull(message = "원본의 게시글 시작일을 입력해주세요")
+        private LocalDate originalPublishedDate;
+
+        @NotNull(message = "추천 대상 최소 1개 선택해주세요.")
+        @Size(min = 1, message = "최소 1개의 추천 대상이 필요합니다.")
+        //"개발자", "AI" 등등
+        private List<String> targetRoles;
+    }
+}
