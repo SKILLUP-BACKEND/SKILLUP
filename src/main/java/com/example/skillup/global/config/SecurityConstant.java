1e889b5375e3e73bc5158b3b5f0943a9850889a1
junwoo <mkshie@naver.com>
2026-01-17 17:00:26 +0900

    refactor : 공개 URL 및 관리자 URL 상수 클래스 추가 및 401 에러 및 403 에러로 보내게끔 수정

---
+++ src/main/java/com/example/skillup/global/config/SecurityConstant.java
@@ -0,0 +1,57 @@
+package com.example.skillup.global.config;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConstant {
+    public static final String[] PUBLIC_AUTH_URLS = {
+
+            // ========== Events (public) ==========
+            "/events/*",                       // GET /events/{eventId}
+            "/events/home/featured",           // GET
+            "/events/home/closing-soon",       // GET
+            "/events/home/category",           // GET
+            "/events/home/banners",            // GET
+            "/events/search/home",             // POST
+
+            // Category page
+            "/events/category-page/search",     // POST
+            "/events/category-page/recommended",// GET
+
+            // Home
+            "/events/home/recent",             // GET
+            "/events/*/apply"  ,                // PATCH /events/{eventId}/apply
+
+            // ========== Articles (public) ==========
+            "/articles/search",                 // GET
+            "/articles",                        // GET
+            "/articles/read/*",                  // POST /articles/read/{articleId}
+
+            // ========== User (public) ==========
+            "/user/test-login",                  // GET (테스트용)
+            "/user/my-page/profile/interest",    // GET
+            "/user/my-page/with-draw/category",   // GET
+
+            // ========== OAuth (public) ==========
+            "/oauth/**"
+    };
+
+    // Swagger UI 관련 공개 경로
+    public static final String[] SWAGGER_URLS = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    };
+
+    // Admin 관련 API 경로
+    public static final String[] ADMIN_URLS = {
+    };
+
+    // 모든 공개 URL들
+    public static final String[] PUBLIC_URLS =
+            Stream.of(PUBLIC_AUTH_URLS, SWAGGER_URLS)
+                    .flatMap(Arrays::stream)
+                    .toArray(String[]::new);
+}
