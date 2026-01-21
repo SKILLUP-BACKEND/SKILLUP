4534aa271445d4487ffe0bf53aacc6d90e9f0b46
kimjunghwannn <aa2002dcks@naver.com>
2026-01-21 23:58:44 +0900

    fix: 마이 페이지 홈 API 프로필 이미지 전달 및 qna 권한 모두 허용으로 변경

--- src/main/java/com/example/skillup/global/config/SecurityConstant.java
+++ src/main/java/com/example/skillup/global/config/SecurityConstant.java
@@ -33,6 +33,7 @@ public class SecurityConstant {
             "/user/test-login",                  // GET (테스트용)
             "/user/my-page/profile/interest",    // GET
             "/user/my-page/with-draw/category",   // GET
+            "/user/my-page/qna",   // GET
 
             // ========== OAuth (public) ==========
             "/oauth/**"
