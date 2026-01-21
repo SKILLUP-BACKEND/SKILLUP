c91250d6e2ce14abd545289371de9c00ec03ce7c
junwoo <mkshie@naver.com>
2026-01-21 20:57:34 +0900

    fix : 최근 조회한 이벤트 조회시 UsersDetails 의 User 에서 targetRole 에 접근 시 에러 발생 repo 에서 값을 찾아오는 것으로 수정

--- src/main/java/com/example/skillup/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/skillup/domain/user/repository/UserRepository.java
@@ -1,62 +1,72 @@
 package com.example.skillup.domain.user.repository;
 
-import com.example.skillup.domain.user.dto.response.UserResponse;
 import com.example.skillup.domain.user.entity.Users;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.userdetails.User;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<Users, Long>
-{
+public interface UserRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByEmail(String email);
+
     Optional<Users> findBySocialId(String socialId);
 
     @Query(
             value = """
-    SELECT *
-    FROM users u
-    WHERE
-        (:deleted = true
-         OR (:deleted = false AND u.deleted_at IS NULL))
-      AND (:keyword IS NULL
-           OR u.email LIKE CONCAT('%', :keyword, '%')
-           OR u.name  LIKE CONCAT('%', :keyword, '%'))
-    ORDER BY u.created_at DESC
-    """,
+                    SELECT *
+                    FROM users u
+                    WHERE
+                        (:deleted = true
+                         OR (:deleted = false AND u.deleted_at IS NULL))
+                      AND (:keyword IS NULL
+                           OR u.email LIKE CONCAT('%', :keyword, '%')
+                           OR u.name  LIKE CONCAT('%', :keyword, '%'))
+                    ORDER BY u.created_at DESC
+                    """,
             nativeQuery = true
     )
-    List<Users> findUsersByKeyWardAndDeleted(@Param("keyWard") String keyWard, @Param("deleted") Boolean deleted,Pageable pageable);
+    List<Users> findUsersByKeyWardAndDeleted(@Param("keyWard") String keyWard, @Param("deleted") Boolean deleted,
+                                             Pageable pageable);
 
     @Query(value = """
-SELECT
-    COALESCE(SUM(ea.action_type = 'VIEW') AS viewCnt,0),
-    COALESCE(SUM(ea.action_type = 'APPLY') AS applyCnt,0),
-    COALESCE(SUM(ea.action_type = 'SAVE') AS saveCnt,0)
-FROM event_action ea
-WHERE ea.actorId = :actorId
-""", nativeQuery = true)
+            SELECT
+                COALESCE(SUM(ea.action_type = 'VIEW') AS viewCnt,0),
+                COALESCE(SUM(ea.action_type = 'APPLY') AS applyCnt,0),
+                COALESCE(SUM(ea.action_type = 'SAVE') AS saveCnt,0)
+            FROM event_action ea
+            WHERE ea.actorId = :actorId
+            """, nativeQuery = true)
     EventActionCountProjection getUserActionCounts(@Param("actorId") String actorId);
+
+    @Query(
+            value = """
+                    SELECT r.name
+                    from Users u
+                    join u.role r
+                    where u = :user
+                    """
+    )
+    String findByRoleNameByUsers(@Param("user") Users user);
 
     public interface EventActionCountProjection {
         int getViewCnt();
+
         int getApplyCnt();
+
         int getSaveCnt();
     }
-    @Query(value = """
-    SELECT COUNT(*)
-    FROM Users u
-    WHERE u.deletedAt IS NULL
-    """)
-    int getTotalCount();
 
+    @Query(value = """
+            SELECT COUNT(*)
+            FROM Users u
+            WHERE u.deletedAt IS NULL
+            """)
+    int getTotalCount();
 
 
     @Query(
@@ -66,13 +76,12 @@ WHERE ea.actorId = :actorId
     Optional<Users> findByIdNative(@Param("userId") Long userId);
 
 
-
     @Modifying
     @Query("""
-    DELETE FROM Users u
-    WHERE u.role = :guestRole
-      AND u.deletedAt IS NOT NULL
-      AND u.deletedAt <= :threshold
-""")
-    void deleteExpiredUsers( @Param("threshold") LocalDateTime threshold);
+                DELETE FROM Users u
+                WHERE u.role = :guestRole
+                  AND u.deletedAt IS NOT NULL
+                  AND u.deletedAt <= :threshold
+            """)
+    void deleteExpiredUsers(@Param("threshold") LocalDateTime threshold);
 }
