5ac909a138170f1fa3b1b46158b12a05c80cb3d6
junwoo <mkshie@naver.com>
2026-02-23 17:52:52 +0900

    fix : NPE 문제 해결

--- src/main/java/com/example/skillup/domain/user/service/UserService.java
+++ src/main/java/com/example/skillup/domain/user/service/UserService.java
@@ -33,7 +33,6 @@ import com.example.skillup.global.service.S3Service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -100,7 +99,8 @@ public class UserService {
                 recruitingEvents.add(event);
             }
         }
-        return userMapper.toMyPageBookMarkResponse(user, recruitingEvents, closedEvents, pageInfoResponse ,eventBookmarks.getTotalElements() );
+        return userMapper.toMyPageBookMarkResponse(user, recruitingEvents, closedEvents, pageInfoResponse,
+                eventBookmarks.getTotalElements());
     }
 
     @Transactional(readOnly = true)
@@ -111,9 +111,9 @@ public class UserService {
     }
 
     @Transactional(readOnly = true)
-    public List<UserResponse.WithDrawReasonCategoryResponse> getWithDrawReasonCategory()
-    {
-        return withDrawReasonCategoryRepository.findAll().stream().map(userMapper::toWithDrawReasonCategoryResponse).toList();
+    public List<UserResponse.WithDrawReasonCategoryResponse> getWithDrawReasonCategory() {
+        return withDrawReasonCategoryRepository.findAll().stream().map(userMapper::toWithDrawReasonCategoryResponse)
+                .toList();
     }
 
     @ConvertNotFound(
@@ -127,9 +127,10 @@ public class UserService {
         user = notFoundGuardService.getUsersNative(user.getId());
         TargetRole role = notFoundGuardService.getRole(request.getRole());
         Set<Interest> interests = notFoundGuardService.getInterestFindByNameIn(request.getInterests());
-        String userProfileImageUrl=null;
-        if(profileImage!=null && !profileImage.isEmpty())
+        String userProfileImageUrl = null;
+        if (profileImage != null && !profileImage.isEmpty()) {
             userProfileImageUrl = s3Service.uploadFile(profileImage, "user/profile");
+        }
         user.update(request, role, interests, userProfileImageUrl);
         return userMapper.toUserProfileResponse(user);
     }
@@ -145,20 +146,18 @@ public class UserService {
     }
 
     @Transactional
-    public void deleteUser(UserRequest.UserWithdrawRequest request,Users user) {
+    public void deleteUser(UserRequest.UserWithdrawRequest request, Users user) {
         user = notFoundGuardService.getUsersNative(user.getId());
         user.withdraw(request.getDetail());
     }
 
     @Transactional
-    public void completeSignup(Users user, UserRequest.UserOAuthSignupRequest request)
-    {
+    public void completeSignup(Users user, UserRequest.UserOAuthSignupRequest request) {
         user = notFoundGuardService.getUsersNative(user.getId());
         TargetRole role = notFoundGuardService.getRole(request.getRole());
         Set<Interest> interests = notFoundGuardService.getInterestFindByNameIn(request.getInterests());
-        user.update(null,role,interests,null);
+        user.update(null, role, interests, null);
     }
-
 
 
     @Transactional
@@ -166,7 +165,7 @@ public class UserService {
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime since = now.minusDays(RETENTION_DAYS);
 
-        recentSearchRepository.deleteExpiredByUserId(userId , since);
+        recentSearchRepository.deleteExpiredByUserId(userId, since);
 
         List<RecentSearch> recentKeywords =
                 recentSearchRepository.findTopByUserIdSinceOrderByUpdatedAtDesc(
@@ -177,22 +176,21 @@ public class UserService {
     }
 
 
-
     @Transactional
     public void saveRecentSearchKeyword(Long userId, String keyword) {
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime since = now.minusDays(RETENTION_DAYS);
 
-        recentSearchRepository.deleteExpiredByUserId(userId , since); // 만료된 데이터 정리
+        recentSearchRepository.deleteExpiredByUserId(userId, since); // 만료된 데이터 정리
 
         String trimmedKeyword = keyword.trim();
 
         RecentSearch recent = recentSearchRepository.findByUserIdAndKeyword(userId, trimmedKeyword)
-                .map(existing ->{
+                .map(existing -> {
                     existing.setUpdatedAt();
                     return existing;
                 })
-                .orElseGet(()-> RecentSearch.builder()
+                .orElseGet(() -> RecentSearch.builder()
                         .userId(userId)
                         .keyword(trimmedKeyword)
                         .build());
@@ -217,15 +215,16 @@ public class UserService {
     }
 
     @Transactional
-    public UserResponse.ContinueLoginResponse continueLogin(UserRequest.ContinueLoginRequest request)
-    {
-        Users user=userRepository.findBySocialLoginTypeAndSocialId
-                (SocialLoginType.valueOf(request.getSocialLoginType()),request.getSocialId()).orElse(null);
+    public UserResponse.ContinueLoginResponse continueLogin(UserRequest.ContinueLoginRequest request) {
+        Users user = userRepository.findBySocialLoginTypeAndSocialId
+                        (SocialLoginType.valueOf(request.getSocialLoginType()), request.getSocialId())
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_ENTITY_NOT_FOUND, "해당 사용자가 존재하지 않습니다."));
 
-        if(user.getStatus()== UserStatus.WITHDRAWN)
+        if (user.getStatus() == UserStatus.WITHDRAWN) {
             user.rejoin();
+        }
 
-        TokenResponse tokenResponse =authService.login(user.getEmail(),"users");
+        TokenResponse tokenResponse = authService.login(user.getEmail(), "users");
 
         return UserResponse.ContinueLoginResponse.builder().accessToken(tokenResponse.accessToken()).build();
     }
