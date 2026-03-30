85fd4d4873cd3ad74a66c476616a43ecf9458c4e
junwoo <mkshie@naver.com>
2026-03-31 00:57:21 +0900

    feat(recovery): S3 Event DTO 의 경우 확장성 고려를 위한 Type 추가

--- src/main/java/com/example/skillup/domain/event/events/ThumbnailReplacedEvent.java
+++ src/main/java/com/example/skillup/domain/event/events/ThumbnailReplacedEvent.java
@@ -1,4 +1,6 @@
 package com.example.skillup.domain.event.events;
 
-public record ThumbnailReplacedEvent(String oldThumbnailUrl) {
+import com.example.skillup.global.recovery.enums.ResourceType;
+
+public record ThumbnailReplacedEvent(ResourceType resourceType,Long resourceId, String oldThumbnailUrl) {
 }
--- src/main/java/com/example/skillup/domain/event/events/ThumbnailUploadedEvent.java
+++ src/main/java/com/example/skillup/domain/event/events/ThumbnailUploadedEvent.java
@@ -1,4 +1,6 @@
 package com.example.skillup.domain.event.events;
 
-public record ThumbnailUploadedEvent(String thumbnailUrl) {
+import com.example.skillup.global.recovery.enums.ResourceType;
+
+public record ThumbnailUploadedEvent(ResourceType resourceType, String thumbnailUrl) {
 }
