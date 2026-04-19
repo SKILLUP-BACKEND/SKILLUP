9d0e3902f074aa8c98d409470824457dc4c92ee3
junwoo <mkshie@naver.com>
2026-04-19 18:36:50 +0900

    fix(event) : event title 생성 시 중복 검사 로직이 있어서 불필요한 unique 제거

--- src/main/java/com/example/skillup/domain/event/entity/Event.java
+++ src/main/java/com/example/skillup/domain/event/entity/Event.java
@@ -41,7 +41,7 @@ public class Event extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column
     private String title;
 
     @Column(length = 512)
